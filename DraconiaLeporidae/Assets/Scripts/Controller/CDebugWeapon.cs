e6bcfd86028be2a538c9d3d2063d6c80de350c25
Alex Zimmer <alexzimmer.zimmer1@gmail.com>
2022-05-23 16:03:10 -0300

    Debug Weapon Controller

---
+++ DraconiaLeporidae/Assets/Scripts/Controller/CDebugWeapon.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CDebugWeapon : MonoBehaviour, IInteract
+{
+
+    public void Awake()
+    {
+        
+    }
+
+    public void OnInteract()
+    {
+      
+    }
+
+    public void Start()
+    {
+        
+    }
+    public void Update()
+    {
+        
+    }
+    
+
+
+    
+}
